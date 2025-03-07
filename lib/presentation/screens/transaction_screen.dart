68099f284013cf84964481cf0354f4a32f13a1d6
josh_MacBook <josuearroliga89@gmail.com>
2025-03-06 22:24:04 -0600

    Added capability to use a number keyboard for the amount section.

--- lib/presentation/screens/transaction_screen.dart
+++ lib/presentation/screens/transaction_screen.dart
@@ -45,186 +45,201 @@ class TransactionScreen extends StatelessWidget {
         appBar: AppBar(
           title: const Text("Add Record"),
         ),
-        body: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 30),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                Center(
-                  child: BlocBuilder<RecordTypeBloc, RecordTypeState>(
-                    builder: (context, state) {
-                      return SegmentedButton<Operations>(
-                        segments: const <ButtonSegment<Operations>>[
-                          ButtonSegment(
-                            value: Operations.income,
-                            label: Text("Income"),
-                          ),
-                          ButtonSegment(
-                            value: Operations.expense,
-                            label: Text("Expense"),
-                          ),
-                          ButtonSegment(
-                            value: Operations.transfer,
-                            label: Text("Transfer"),
-                          )
-                        ],
-                        selected: <Operations>{
-                          state.selectedValue as Operations
-                        },
-                        onSelectionChanged: (selected) {
-                          context.read<RecordTypeBloc>().add(
-                              RecordChanged(selectedRecord: selected.first));
-                          print(selected);
-                        },
-                      );
-                    },
-                  ),
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                //DATE SECTION
-                Center(
-                  child: BlocBuilder<DatePickerBloc, DatePickerState>(
-                    builder: (context, state) {
-                      return GestureDetector(
-                        onTap: () async {
-                          DateTime? pickedDate = await showDatePicker(
-                              context: context,
-                              firstDate: DateTime(2000),
-                              lastDate: DateTime(2100));
-                          //
-                          if (context.mounted && pickedDate != null) {
-                            context
-                                .read<DatePickerBloc>()
-                                .add(DateChanged(selectedDate: pickedDate));
-                          }
-                        },
-                        child: Chip(
-                          avatar: const Icon(Icons.calendar_month),
-                          label: Text(DateFormat.yMMMd().format(context
-                              .read<DatePickerBloc>()
-                              .state
-                              .selectedDate)),
-                        ),
-                      );
-                    },
-                  ),
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                const Row(
-                  children: [
-                    Chip(label: Text("USD")),
-                    SizedBox(
-                      width: 10,
-                    ),
-                    Flexible(
-                      child: TextField(
-                        textAlign: TextAlign.end,
-                      ),
-                    )
-                  ],
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-
-                ///General Section
-
-                const TextField(
-                  decoration: InputDecoration(
-                    labelText: 'Description',
-                    hintText: 'Enter a description',
-                  ),
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-
-                const SizedBox(
-                  height: 10,
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    const SizedBox(
-                        child: Row(
-                      children: [
-                        Icon(Icons.wallet),
-                        SizedBox(
-                          width: 10,
-                        ),
-                        Text('Account'),
-                      ],
-                    )),
-                    BlocBuilder<AccountBloc, AccountState>(
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 30),
+          child: Column(
+            children: [
+              Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Center(
+                    child: BlocBuilder<RecordTypeBloc, RecordTypeState>(
                       builder: (context, state) {
-                        return DropdownMenu<String>(
-                            initialSelection: state.selectedAccount,
-                            dropdownMenuEntries: state.accounts
-                                .map(
-                                  (String option) => DropdownMenuEntry(
-                                      value: option, label: option),
-                                )
-                                .toList());
+                        return SegmentedButton<Operations>(
+                          segments: const <ButtonSegment<Operations>>[
+                            ButtonSegment(
+                              value: Operations.income,
+                              label: Text("Income"),
+                            ),
+                            ButtonSegment(
+                              value: Operations.expense,
+                              label: Text("Expense"),
+                            ),
+                            ButtonSegment(
+                              value: Operations.transfer,
+                              label: Text("Transfer"),
+                            )
+                          ],
+                          selected: <Operations>{
+                            state.selectedValue as Operations
+                          },
+                          onSelectionChanged: (selected) {
+                            context.read<RecordTypeBloc>().add(
+                                RecordChanged(selectedRecord: selected.first));
+                            print(selected);
+                          },
+                        );
                       },
                     ),
-                  ],
-                ),
-                const SizedBox(
-                  height: 30,
-                ),
-                BlocBuilder<CategoryBloc, CategoryState>(
-                  builder: (context, state) {
-                    return GridView.builder(
-                      shrinkWrap: true,
-                      physics: const NeverScrollableScrollPhysics(),
-                      gridDelegate:
-                          const SliverGridDelegateWithFixedCrossAxisCount(
-                        crossAxisCount: 3, // Number of columns
-                        crossAxisSpacing:
-                            20.0, // Horizontal spacing between items
-                        mainAxisSpacing: 20.0, // Vertical spacing between items
-                      ),
-                      itemCount: state.categories.length,
-                      itemBuilder: (context, index) {
-                        final category = state.categories[index];
-                        final iconName = category.icon;
-                        return InkWell(
-                          onTap: () {},
-                          borderRadius: BorderRadius.circular(20),
-                          splashColor: const Color.fromARGB(255, 156, 143, 193),
-                          child: Container(
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(20),
-                              border: Border.all(
-                                color: Colors.grey,
-                                width: 0.5,
-                              ),
-                            ),
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.center,
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              mainAxisSize: MainAxisSize.min,
-                              children: [
-                                IconGrabber(iconName: iconName),
-                                Text(category.name),
-                              ],
-                            ),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  //DATE SECTION
+                  Center(
+                    child: BlocBuilder<DatePickerBloc, DatePickerState>(
+                      builder: (context, state) {
+                        return GestureDetector(
+                          onTap: () async {
+                            DateTime? pickedDate = await showDatePicker(
+                                context: context,
+                                firstDate: DateTime(2000),
+                                lastDate: DateTime(2100));
+                            //
+                            if (context.mounted && pickedDate != null) {
+                              context
+                                  .read<DatePickerBloc>()
+                                  .add(DateChanged(selectedDate: pickedDate));
+                            }
+                          },
+                          child: Chip(
+                            avatar: const Icon(Icons.calendar_month),
+                            label: Text(DateFormat.yMMMd().format(context
+                                .read<DatePickerBloc>()
+                                .state
+                                .selectedDate)),
                           ),
                         );
                       },
-                    );
-                  },
-                )
-              ],
-            ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  const Row(
+                    children: [
+                      Chip(label: Text("USD")),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      Flexible(
+                        child: TextField(
+                          textAlign: TextAlign.end,
+                          keyboardType: TextInputType
+                              .number, // Set the keyboard type to number
+                          decoration: InputDecoration(
+                            labelText: 'Amount',
+                            hintText: 'Enter amount',
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+
+                  ///General Section
+
+                  const TextField(
+                    decoration: InputDecoration(
+                      labelText: 'Description',
+                      hintText: 'Enter a description',
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      const SizedBox(
+                          child: Row(
+                        children: [
+                          Icon(Icons.wallet),
+                          SizedBox(
+                            width: 10,
+                          ),
+                          Text('Account'),
+                        ],
+                      )),
+                      BlocBuilder<AccountBloc, AccountState>(
+                        builder: (context, state) {
+                          return DropdownMenu<String>(
+                              initialSelection: state.selectedAccount,
+                              dropdownMenuEntries: state.accounts
+                                  .map(
+                                    (String option) => DropdownMenuEntry(
+                                        value: option, label: option),
+                                  )
+                                  .toList());
+                        },
+                      ),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                ],
+              ),
+              //Scrollable GRID
+              Expanded(
+                child: SingleChildScrollView(
+                  child: BlocBuilder<CategoryBloc, CategoryState>(
+                    builder: (context, state) {
+                      return GridView.builder(
+                        shrinkWrap: true,
+                        physics: const NeverScrollableScrollPhysics(),
+                        gridDelegate:
+                            const SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 3, // Number of columns
+                          crossAxisSpacing:
+                              20.0, // Horizontal spacing between items
+                          mainAxisSpacing:
+                              20.0, // Vertical spacing between items
+                        ),
+                        itemCount: state.categories.length,
+                        itemBuilder: (context, index) {
+                          final category = state.categories[index];
+                          final iconName = category.icon;
+                          return InkWell(
+                            onTap: () {},
+                            borderRadius: BorderRadius.circular(20),
+                            splashColor:
+                                const Color.fromARGB(255, 156, 143, 193),
+                            child: Container(
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(20),
+                                border: Border.all(
+                                  color: Colors.grey,
+                                  width: 0.5,
+                                ),
+                              ),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                mainAxisSize: MainAxisSize.min,
+                                children: [
+                                  IconGrabber(iconName: iconName),
+                                  Text(category.name),
+                                ],
+                              ),
+                            ),
+                          );
+                        },
+                      );
+                    },
+                  ),
+                ),
+              ),
+            ],
           ),
         ),
       ),
